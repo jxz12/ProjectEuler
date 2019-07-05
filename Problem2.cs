ae40cf97b2b6ec29af775633a0846d7559b21627
jxz12 <jxz12@ic.ac.uk>
2019-07-05 18:02:27 +0100

    solved 2

---
+++ Problem2.cs
@@ -0,0 +1,31 @@
+using System;
+
+class Problem2
+{
+    static void Main(String[] args)
+    {
+        int t = Convert.ToInt32(Console.ReadLine());
+        for (int a0 = 0; a0 < t; a0++)
+        {
+            long n = Convert.ToInt64(Console.ReadLine());
+
+            Console.WriteLine(EvenFibonacciNaive(n));
+        }
+    }
+    static long EvenFibonacciNaive(long n)
+    {
+        long curr = 1, prev = 0;
+        long sum = 0;
+        while (curr<n)
+        {
+            if (curr % 2 == 0)
+            {
+                sum += curr;
+            }
+            long next = curr + prev;
+            prev = curr;
+            curr = next;
+        }
+        return sum;
+    }
+}
