228c34d6668bd32f651015f27c116da0efa52472
jxz12 <jxz12@ic.ac.uk>
2019-07-08 00:54:55 +0100

    done Problem 4

---
+++ Problem4.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Text;
+
+class Solution
+{
+    static void Main(string[] args)
+    {
+        int t = Convert.ToInt32(Console.ReadLine());
+        for (int a0 = 0; a0 < t; a0++)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int m = int.Parse(n.ToString().Substring(0,3));
+            if (Palindromise(m) >= n)
+            {
+                m -= 1;
+            }
+
+            for (int i=m; i>=101; i--)
+            {
+                int palindrome = Palindromise(i);
+                if (SmallestFactor(palindrome) != -1)
+                {
+                    Console.WriteLine(palindrome);
+                    break;
+                }
+            }
+        }
+    }
+    static int Palindromise(int input)
+    {
+        var sb = new StringBuilder();
+        string inString = input.ToString();
+        sb.Append(inString);
+        for (int i=inString.Length-1; i>=0; i--)
+        {
+            sb.Append(inString[i]);
+        }
+        return int.Parse(sb.ToString());
+    }
+    static int SmallestFactor(int input)
+    {
+        for (int i=100; i<1000; i++)
+        {
+            if (input%i==0 && input/i<1000)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
