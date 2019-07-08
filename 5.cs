3591faa098e4a09557c4134f50bbf18265ad1a8f
jxz12 <jxz12@ic.ac.uk>
2019-07-08 20:50:40 +0100

    solved 5

---
+++ 5.cs
@@ -0,0 +1,29 @@
+using System;
+
+class Solution
+{
+    static void Main(string[] args)
+    {
+        int t = Convert.ToInt32(Console.ReadLine());
+        for(int a0 = 0; a0 < t; a0++)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int numerator = n;
+            while (!TestQuotients(n, numerator))
+            {
+                numerator += n; // only test multiples of the biggest number
+            }
+            Console.WriteLine(numerator);
+        }
+    }
+    static bool TestQuotients(int n, int numerator)
+    {
+        for (int i=1; i<=n; i++)
+        {
+            if (numerator % i != 0)
+                return false;
+        }
+        return true;
+    }
+}
