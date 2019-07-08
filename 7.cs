0d4c483fea65f1f66c7d7bde8f9b91c4d988cfcb
jxz12 <jxz12@ic.ac.uk>
2019-07-08 21:12:53 +0100

    Solved 7

---
+++ 7.cs
@@ -0,0 +1,44 @@
+using System;
+
+class Problem7
+{
+    static void Main(string[] args)
+    {
+        int t = int.Parse(Console.ReadLine());
+
+        int[] primes = NthPrimes(10000); // hacky, but in the spec
+        for(int a0 = 0; a0 < t; a0++)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine(primes[n-1]);
+        }
+    }
+    static int[] NthPrimes(int n)
+    {
+        int[] allPrimes = new int[n];
+        allPrimes[0] = 2;
+
+        int nextAttempt = 1;
+        int i = 1;
+        while (i < n)
+        {
+            nextAttempt += 2;
+            if (IsPrime(nextAttempt))
+            {
+                allPrimes[i++] = nextAttempt;
+            }
+        }
+        return allPrimes;
+    }
+    static bool IsPrime(int n)
+    {
+        for (int i=2; i<=Math.Sqrt(n); i++)
+        {
+            if (n % i == 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
