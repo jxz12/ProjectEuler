9beb32c782eb113fc62489192409e80273370bd4
jxz12 <jxz12@ic.ac.uk>
2019-07-09 21:58:50 +0100

    Create 10.cs

---
+++ 10.cs
@@ -0,0 +1,40 @@
+using System;
+
+class Problem10
+{
+    static void Main(string[] args)
+    {
+        int t = int.Parse(Console.ReadLine());
+        long[] cumul = PrimesCumulative(1000000);
+        for(int a0 = 0; a0 < t; a0++)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine(cumul[n-1]);
+        }
+    }
+    static long[] PrimesCumulative(int n)
+    {
+        long[] cumul = new long[n];
+        cumul[0] = 0; // 1 is not prime
+        cumul[1] = 2; // 2 is prime
+
+        long sum = 2;
+        for (int i=2; i<n; i++)
+        {
+            if (IsPrime(i+1))
+                sum += i+1;
+
+            cumul[i] = sum;
+        }
+        return cumul;
+    }
+    static bool IsPrime(int n)
+    {
+        for (int i=2; i<Math.Sqrt(n)+1; i++)
+        {
+            if (n % i == 0)
+                return false;
+        }
+        return true;
+    }
+}
