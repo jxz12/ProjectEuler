f2ee6e88688152bb932a5761595630a76d562c2b
Jonathan Zheng <jxz12@ic.ac.uk>
2019-08-14 11:28:41 +0100

    Dynamic programming is beautiful! But hard...

--- 31.cs
+++ 31.cs
@@ -4,7 +4,7 @@ class Problem31
 {
     static void Main(string[] args)
     {
-        int[] coins = new int[]{ 1,2,5,10,20,50,100,200 };
+        var coins = new int[]{ 1,2,5,10,20,50,100,200 };
         var memo = Change(100001, coins);
 
         int t = int.Parse(Console.ReadLine());
