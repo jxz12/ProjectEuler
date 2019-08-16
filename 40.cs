587a4ce8aaaef09afe665e9f9b43799ca5231784
Jonathan Zheng <jxz12@ic.ac.uk>
2019-08-16 12:47:46 +0100

    yay

--- 40.cs
+++ 40.cs
@@ -1,17 +1,19 @@
 using System;
 
-class Problem1
+class Problem40
 {
     static void Main(string[] args)
     {
         int t = int.Parse(Console.ReadLine());
         while (t-- > 0)
         {
-            long[] i_n = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
-            for (ing i=0; i<7; i++)
+            long[] i_n = Array.ConvertAll(Console.ReadLine().Split(' '), long.Parse);
+            int ans = 1;
+            for (int n=0; n<i_n.Length; n++)
             {
-                Console.WriteLine(i_n[i]);
+                ans *= Champernowne(i_n[n]-1) - '0';
             }
+            Console.WriteLine(ans);
         }
     }
     static char Champernowne(long i)
@@ -19,16 +21,18 @@ class Problem1
         // remove one 'set' of numbers at a time
         // a set is the sequence of numbers with the same no digits
         int nDigits = 0;
-        while (i > 0)
+        while (i >= 0)
         {
             nDigits += 1;
-            i -= nDigits * 9 * (long)Math.Pow(10, i-1);
+            i -= nDigits * 9 * (long)Math.Pow(10, nDigits-1);
         }
-        i += nDigits * 9 * (long)Math.Pow(10, i-1);
+        i += nDigits * 9 * (long)Math.Pow(10, nDigits-1);
 
-        long number = i / nDigits;
-        long digit = i % nDigits;
+        long order = i / nDigits; // its order within this set
+        long digit = i % nDigits; // the digit within the number
 
-        return number.ToString()[digit];
+        long number = (long)Math.Pow(10,nDigits-1) + order;
+
+        return number.ToString()[(int)digit];
     }
 }
