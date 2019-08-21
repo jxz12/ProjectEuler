aea0ab7cf015fb51a04e438c811560e2a2c56447
Jonathan Zheng <jxz12@ic.ac.uk>
2019-08-21 22:47:25 +0100

    Read the spec! Anyway, nice one to finish on :)

--- 50.cs
+++ 50.cs
@@ -5,7 +5,7 @@ class Problem50
 {
     static void Main(string[] args)
     {
-        List<long> primes = Erastosthenes(6000000); // stupid magic number
+        List<long> primes = Erastosthenes(6000000); // kinda stupid magic number
 
         int t = int.Parse(Console.ReadLine());
         while (t-- > 0)
@@ -14,28 +14,26 @@ class Problem50
 
             long bestSum = 2;
             int bestSteps = 1;
-            long bestStart = 2;
             for (int i=0; i<primes.Count; i++)
             {
-                if (primes[i] > 131) break; // stupid magic number
+                if (primes[i] > 131) break; // really stupid magic number
 
-                // find biggest sum
+                // for each starting prime, find biggest sum below n
                 long sum = 0;
                 int j = i;
-                while (sum + primes[j] < n)
+                while (sum + primes[j] <= n)
                 {
                     sum += primes[j++];
                 }
 
                 // decrement until a prime is found
                 int steps = j-i;
-                while (steps > bestSteps)
+                while (steps > bestSteps || (steps==bestSteps && sum<bestSum))
                 {
                     if (IsPrime(sum, primes))
                     {
                         bestSum = sum;
                         bestSteps = steps;
-                        bestStart = primes[i];
                         break;
                     }
                     j -= 1;
